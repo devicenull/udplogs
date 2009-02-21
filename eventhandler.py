3b151b8d9f86b554d9c9550c5b39c700a4c57352
devicenull <devicenull@xaphan.(none)>
2009-02-20 21:17:03 -0500

    Added callback system to eventhandler

--- eventhandler.py
+++ eventhandler.py
@@ -5,20 +5,29 @@ event_log = logging.getLogger("event_log")
 
 class EventHandler:
 	def __init__(self):
-		self.events = []
+		self.events = {}
 
 	def registerEvent(self,newevent):
-		self.events.append(newevent)
-		main_log.debug("Registered event %s" % newevent)
+		eventname = newevent.__name__.split(".")[-1]
+		self.events[eventname] = {"event":newevent,"callbacks":[]}
+		main_log.debug("Registered event %s" % eventname)
 
-	def registerCallback(self,callback,events):
-		pass
+	def registerCallback(self,callback,cb_events):
+		for cur in cb_events:
+			if events.has_key[cur]:
+				events[cur]['callbacks'].append(callback)
+				main_log.debug("Registered callback %s for event %s" % (callback,cur))
+			else:
+				main_log.warning("Attempted to register callback %s for unknown event %s" % (callback,cur))
 
-	def event(self,instr):
+	def event(self,instr,ip="127.0.0.1",port=27015,timestamp="sometime"):
 		instr = instr.strip()
 
 		for cur in self.events:
-			if cur.isMatch(instr):
-				event_log.debug(cur(instr))
+			if self.events[cur]['event'].isMatch(instr):
+				theevent = self.events[cur]['event'](instr)
+				event_log.debug(theevent)
+				for cur_cb in self.events[cur]['callbacks']:
+					cur_cb(theevent,ip,port,timestamp)
 
 eventhandler = EventHandler()
