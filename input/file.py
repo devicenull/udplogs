e3b94b5ed54ec35d28bdc4b5728944ecc657ce5e
devicenull <devicenull@xaphan.(none)>
2009-02-20 19:03:52 -0500

    Made file input source strip off timestamp

--- input/file.py
+++ input/file.py
@@ -7,11 +7,14 @@ class FileInput:
 		self.input = open(filename,"r")
 		self.eventHandler = eventHandler
 		main_log.debug("FileInput init, eventHandler=%s log source=%s" % (eventHandler,filename))
-	
+
+	# example line:
+	# L 02/05/2009 - 01:04:11: "[SC-UBW] Yahn<1620><BOT><>" connected, address "none"
+	# timestamp ends at char 25	
 	def start(self):
 		main_log.debug("FileInput starting!")
 		for curLine in self.input:
-			self.eventHandler.event(curLine)
+			self.eventHandler.event(curLine[25:])
 
 	def stop(self):
 		main_log.debug("FileInput stopping!")
